54e9156449c5d37e607bddd8fdc72d1b527c413c
dereckc210 <dereck.pirulin@icloud.com>
2025-05-02 13:01:59 -0400

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,10 @@ let energy = 5;
 let happiness = 5;
 
 function myHunger() {
-  myHunger -= 1;
-  console.log("You fed your pet! Hunger is now" + myHunger);
+  hunger -= 1;
+  console.log("You fed your pet! Hunger is now" + hunger);
+}
+
+if (hunger<= 0) {
+  console.log("Your pet is full and doesn't want to eat more!");
 }
