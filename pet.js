e2a84c19cfa5fe4e27602c32e6eaa250f741de42
dereckc210 <dereck.pirulin@icloud.com>
2025-05-19 12:16:25 -0400

    Update pet.js

--- pet.js
+++ pet.js
@@ -2,11 +2,31 @@ let hunger = 5;
 let energy = 5;
 let happiness = 5;
 
-function myHunger() {
+function myfunction () {
   hunger -= 1;
-  console.log("You fed your pet! Hunger is now" + hunger);
+  console.log("You fed your pet! Hunger is now" +hunger)
 }
 
-if (hunger<= 0) {
-  console.log("Your pet is full and doesn't want to eat more!");
+if (hunger <= 0) {
+  console.log("Your pet is full and doesnt want to eat more!")
 }
+
+function playPet() {
+  if (energy > 0) {
+    energy -= 1;
+    energy += 1;
+    console.log("You played with your pet! Energy: " + energy + ",Happiness" + happiness);
+  } else {
+    console.log("Your pet is too tired to play.")
+  }
+}
+
+function sleepPet() {
+  energy += 2;
+  console.log("Your pet took a nap! Energy is now " + energy);
+}
+
+setInterval(function() {
+  hunger += 1;
+  console.log("Your pet is getting Hungrier! Hunger is now " + hunger);
+}, 10000);
